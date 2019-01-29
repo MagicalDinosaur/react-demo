6cdd14bc6523746c3f4522d88d39637187b1fd80
honghui <252860883@qq.com>
2019-01-29 17:21:28 +0800

    添加组件

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+// import App from './components/App'
 import registerServiceWorker from './registerServiceWorker';
 
 // redux
